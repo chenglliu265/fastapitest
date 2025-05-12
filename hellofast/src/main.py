b8fe053d3f80fb8c1e91bde3269a19637c8b15b9
chenglliu265 <chenglliu265@gmail.com>
2025-05-12 23:45:27 +0900

    テストケース追加

--- hellofast/src/main.py
+++ hellofast/src/main.py
@@ -1,12 +1,12 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 from pydantic import BaseModel, Field
 from typing import Annotated
 
 
 class Item(BaseModel):
     name: str
-    desc: str
-    price: float
+    desc: str | None = Field(default=None, title="The description of the item", max_length=300)
+    price: float = Field(gt=0, description="The price must be greater than zero")
     tax: float | None = None
 
 
@@ -27,9 +27,7 @@ async def create_item(item_id: int, item: Item):
     return {"item_id": item_id, **item.model_dump()}
 
 
-@app.put("/items/")
-async def update_item(q: QBody):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q.q:
-        results.update({"q": q.q})
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
     return results
