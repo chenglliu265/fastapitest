d9df1aff53897ba69e373f69534f3f7ba21edc5a
liuchb <liuchb@revolvesys.co.jp>
2025-05-13 10:36:52 +0900

    delete

--- hellofast/src/main.py
+++ hellofast/src/main.py
@@ -5,7 +5,9 @@ from typing import Annotated
 
 class Item(BaseModel):
     name: str
-    desc: str | None = Field(default=None, title="The description of the item", max_length=300)
+    desc: str | None = Field(
+        default=None, title="The description of the item", max_length=300
+    )
     price: float = Field(gt=0, description="The price must be greater than zero")
     tax: float | None = None
 
@@ -31,3 +33,28 @@ async def create_item(item_id: int, item: Item):
 async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
     results = {"item_id": item_id, "item": item}
     return results
+
+
+class QItem(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: QItem, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
+
+
+@app.delete("/items/{item_id}")
+async def del_item(item_id: int):
+    result = [
+        {"item_id": 1, "name": "Python", "desc": "", "price": 12.88, "tax": 1.35},
+        {"item_id": 2, "name": "Java", "desc": "", "price": 12.98, "tax": 1.27},
+    ]
+    result = [item for item in result if item["item_id"] != item_id]
+    return result
